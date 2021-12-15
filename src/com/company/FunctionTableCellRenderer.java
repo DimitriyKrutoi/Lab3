da4b4ba6f263a123b391b95c2bbcf5485ffb6e87
Dima <you@example.com>
2021-12-16 02:56:01 +0300

    red1

--- src/com/company/FunctionTableCellRenderer.java
+++ src/com/company/FunctionTableCellRenderer.java
@@ -1,4 +1,50 @@
 package com.company;
 
-public class FunctionTableCellRenderer {
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+
+public class FunctionTableCellRenderer implements TableCellRenderer {
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+    private String needle = null;
+    //Ищем ячейки, строковое представление которых совпадает с needle
+    // (иголкой). Применяется аналогия поиска иголки в стоге сена, в роли
+    // стога сена - таблица
+
+    private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+
+    public FunctionTableCellRenderer() {
+        panel.add(label);
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT)); // Установить выравнивание надписи по левому краю панели
+        formatter.setMaximumFractionDigits(5); // показать только 5 знаков после запятой
+        formatter.setGroupingUsed(false);
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.'); // Установить в качестве разделителя дробной части точку, а не запятую.
+        formatter.setDecimalFormatSymbols(dottedDouble); // Разместить надпись внутри панели
+    }
+
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean  isSelected, boolean hasFocus, int row, int col) {
+        // Преобразовать double в строку с помощью форматировщика
+        String formattedDouble = formatter.format(value);
+        // Установить текст надписи равным строковому представлению числа
+        label.setText(formattedDouble);
+        if (col == 1 && needle != null && needle.equals(formattedDouble)) {
+            // Номер столбца = 1 (т.е. второй столбец) + иголка не null
+            // (значит что-то ищем) +
+            // значение иголки совпадает со значением ячейки таблицы -
+            panel.setBackground(Color.RED);//красный цвет задний фон панели
+        } else {
+            panel.setBackground(Color.WHITE);//белый цвет
+        }
+        return panel;
+    }
 }
